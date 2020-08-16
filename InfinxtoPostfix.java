76739d8a646d349697155041978802486bdd5770
ShubhamTaneja7 <68348346+ShubhamTaneja7@users.noreply.github.com>
2020-08-16 23:11:17 +0530

    Add files via upload

---
+++ InfinxtoPostfix.java
@@ -0,0 +1,62 @@
+import java.util.Stack;
+class InfextoPostfix{
+   public static void main(String argr[])
+   {
+       String exp="a+b*(c^d-e)^(f+g*h)-i";
+       System.out.println(infextopostfix(exp));
+   }
+   static int precedence(char c){
+    switch (c){
+        case '+':
+        case '-':
+            return 1;
+        case '*':
+        case '/':
+            return 2;
+        case '^':
+            return 3;
+    }
+    return -1;
+}
+   static String infextopostfix(String exp)
+    {
+        String result=new String("");
+        Stack <Character> stack=new Stack<>();
+        int i=0;
+        char c;
+        for(i=0;i<exp.length();i++)
+        {
+            c=exp.charAt(i);
+            if(Character.isLetterOrDigit(c))
+            {
+                result=result+c;
+            }
+            else if(c=='(')
+            {
+                stack.push(c);
+            }
+            else if(c==')')
+            {
+                while(!stack.isEmpty()&&stack.peek()!='(')
+                {
+                    result=result+stack.pop();
+                }
+                stack.pop();
+            }
+            else{
+                while(!stack.isEmpty()&&precedence(c)<=precedence(stack.peek()))
+                {
+                    result=result+stack.pop();
+                }
+                stack.push(c);
+                }
+            }
+        for(i=0;i<=stack.size();i++)
+        {
+            result=result+stack.pop();
+        }
+        return result;
+    }
+
+    
+}
